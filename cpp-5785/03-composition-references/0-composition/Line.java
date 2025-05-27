993fabdc892224997d86c3c8b77188ca328d7eef
mishaask <mishaaskarov@gmail.com>
2025-05-27 11:04:48 +0300

    removed *:Zone.Identifier

---
+++ cpp-5785/03-composition-references/0-composition/Line.java
@@ -0,0 +1,24 @@
+public class Line
+{
+	private Point p1 ;
+	private Point p2;
+
+	public Line() {
+        p1 = new Point();
+        p2 = new Point();
+    }
+
+	public void set(int x1,int y1,int x2,int y2) {
+		p1.setX(x1);
+		p1.setY(y1);
+		p2.setX(x2);
+		p2.setY(y2);
+	}
+
+	public void set(Point p1, Point p2) {
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+
+	public String toString() { 	return "Line from " + p1  + " to " + p2; }
+};
